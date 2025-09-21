e04bdb085354e3448fbfe5e358e810c84e664627
karaflito <michaelxara@gmail.com>
2025-09-21 23:25:33 +0100

    Added climb and changed somethings on the player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,23 +11,28 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float jumpForce = 5f;
     [SerializeField] private float flipDuration = 0.2f;
     [SerializeField] private float inputDeadzone = 0.1f;
+    [SerializeField] private float climbSpeed = 1.0f;
 
-
+    [SerializeField] private Transform visualsTranform;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private float groundCheckRadius = 0.2f;
     [SerializeField] private LayerMask groundLayer;
     private bool isGrounded;
+    private bool isClimbing;
+    private bool isNearClimable;
+    private float savedGravity = 1f;
     // =========================
     // Input
     // =========================
     private PlayerInputActions actions;
     private float moveInput;
+    private float climbingInput;
 
     // =========================
     // Components
     // =========================
     private Rigidbody2D rb;
-    private Animator animator;
+    [SerializeField] private Animator animator;
 
     // =========================
     // Flip State
@@ -42,7 +47,7 @@ public class PlayerMovement : MonoBehaviour
     {
         actions = new PlayerInputActions();
         rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     private void Update()
@@ -54,6 +59,12 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         PlayerMovementVelocity();
+        if (isNearClimable && !isClimbing && Mathf.Abs(climbingInput) > 0.01f)
+        {
+            StartClimb();
+        }
+            
+        PlayerClimbing();
         HandleFlip();
     }
 
@@ -62,8 +73,10 @@ public class PlayerMovement : MonoBehaviour
         actions.Player.Enable();
         actions.Player.Move.performed += OnMove;
         actions.Player.Move.canceled += OnMove;
-
         actions.Player.Jump.performed += OnJump;
+        actions.Player.Climbing.performed += OnClimbing;
+        actions.Player.Climbing.canceled += OnClimbing;
+        
     }
 
     private void OnDisable()
@@ -71,7 +84,8 @@ public class PlayerMovement : MonoBehaviour
         actions.Player.Disable();
         actions.Player.Move.performed -= OnMove;
         actions.Player.Move.canceled -= OnMove;
-
+        actions.Player.Climbing.performed -= OnClimbing;
+        actions.Player.Climbing.canceled -= OnClimbing;
         actions.Player.Jump.performed -= OnJump;
     }
 
@@ -83,14 +97,34 @@ public class PlayerMovement : MonoBehaviour
     private void OnMove(InputAction.CallbackContext ctx)
     {
         moveInput = ctx.ReadValue<float>();
+        
     }
 
     private void OnJump(InputAction.CallbackContext ctx)
     {
+        if (!ctx.performed) return;
+
+        if(isClimbing)
+        {
+            StopClimb();
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            return;
+        }
+        else if (isNearClimable && isGrounded)
+        {
+            PlayerJumping();
+            return;
+        }
         PlayerJumping();
-        Debug.Log("Jump pressed");
+
+
     }    
 
+    private void OnClimbing (InputAction.CallbackContext ctx)
+    {
+        climbingInput = ctx.ReadValue<float>();        
+    }
     // =========================
     // Movement & Animation
     // =========================
@@ -101,20 +135,38 @@ public class PlayerMovement : MonoBehaviour
 
     private void PlayerJumping()
     {
-        if(isGrounded)
+        if (isGrounded)
         {
+            rb.gravityScale = 1f;
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
-        
+
     }
-    private void OnDrawGizmosSelected()
+
+    private void PlayerClimbing()
     {
-        if (groundCheck != null)
-        {
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
-        }
+        if (!isClimbing) return;
+         
+        rb.linearVelocity = new Vector2 (rb.linearVelocity.x, climbingInput * climbSpeed);
+
+        if (!isNearClimable) StopClimb();
     }
+    
+    private void StartClimb()
+    {
+        isClimbing = true;
+        savedGravity = rb.gravityScale;
+        rb.gravityScale = 0;
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x,0f);
+    }
+
+    private void StopClimb()
+    {
+        if (!isClimbing) return;
+        isClimbing = false;
+        rb.gravityScale = savedGravity;
+    }
+   
 
     private void PlayerAnimation()
     {
@@ -144,20 +196,58 @@ public class PlayerMovement : MonoBehaviour
     {
         facingRight = faceRight;
 
-        float start = transform.localScale.x;
+        float start = visualsTranform.localScale.x;
         float end = faceRight ? 1f : -1f;
         float elapsed = 0f;
 
         while (elapsed < flipDuration)
         {
             float newX = Mathf.Lerp(start, end, elapsed / flipDuration);
-            transform.localScale = new Vector3(newX, 1f, 1f);
+            visualsTranform.localScale = new Vector3(newX, 1f, 1f);
 
             elapsed += Time.deltaTime;
             yield return null;
         }
 
-        transform.localScale = new Vector3(end, 1f, 1f);
+        visualsTranform.localScale = new Vector3(end, 1f, 1f);
         flipCoroutine = null;
     }
+    // =========================
+    // Utilities
+    // =========================
+    private void OnDrawGizmosSelected()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        }
+    }
+    // =========================
+    // Colliders
+    // =========================
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Climbing"))
+        {
+            isNearClimable = true; 
+        }
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Climbing"))
+        {
+            isNearClimable = false;
+            StopClimb();
+        }
+    }
+
+
+
+
+
+
+
 }
